9906b47673e18d4052b7d449f050941dda028cc1
Guilherme <guilherme.nmf@gmail.com>
2022-07-14 14:52:05 -0300

    initial commit

---
+++ src/main/java/com/amq/guideline/jms/Consumer.java
@@ -0,0 +1,34 @@
+package com.amq.guideline.jms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import com.amq.guideline.model.Customer;
+
+@Component
+public class Consumer {
+  
+  private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+  @Autowired
+  private JmsTemplate jmsTemplate;
+
+  // Consuming all messages from default queue (testQueue)
+  @JmsListener(destination = "${spring.activemq.default.queue}", containerFactory = "myFactory")
+  public void receiveMessage(Customer customer){
+    logger.info("Consuming message");
+    logger.info(customer.toString());
+  }
+
+  // Receive the last message from a custom queue
+  public void receiveLastMessage(String queue){
+    Customer customer = (Customer)jmsTemplate.receiveAndConvert(queue);
+    logger.info("Consuming message");
+    logger.info(customer.toString());
+  }
+
+}
---
+++ src/main/java/com/amq/guideline/rs/Controller.java
@@ -0,0 +1,57 @@
+package com.amq.guideline.rs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amq.guideline.api.CustomerCreator;
+import com.amq.guideline.jms.Consumer;
+import com.amq.guideline.jms.Producer;
+import com.amq.guideline.model.Customer;
+
+@RestController
+@RequestMapping("/transaction")
+public class Controller {
+
+  @Autowired
+  private Producer producer;
+
+  @Autowired
+  private CustomerCreator customerCreator;
+
+  @Autowired
+  private Consumer consumer;
+
+  // Send a message to default Queue (testQueue)
+  @PostMapping("/send")
+  public void send(@RequestBody Customer customer){
+
+    producer.sendMessage(customer);
+  }
+
+  // Send to a custom queue
+  @PostMapping("/send/{queue}")
+  public void send(@PathVariable("queue") String queue, @RequestBody Customer customer){
+
+    producer.sendMessage(queue, customer);
+  }
+
+  // Send 'n' message to the default queue (testQueue)
+  @PostMapping("/send/many/{n}")
+  public void sendMany(@PathVariable("n") int n){
+
+    customerCreator.createMany(n);
+  }
+
+  // Receive the last message from a custom queue
+  @GetMapping("/receive/{queue}")
+  public void receiveMessage(@PathVariable("queue") String queue){
+
+    consumer.receiveLastMessage(queue);
+  }
+  
+}
